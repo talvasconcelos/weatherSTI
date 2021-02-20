cf0d88d345de9e859339936f2031cbfcb5d36c7d
Tiago Vasconcelos <tvasconcelos@gmail.com>
2021-02-20 15:36:53 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './style';
+
+export default function App() {
+	return (
+		<div className='container'>
+			<header className="title">
+				<h1>weather forecast</h1>
+				<h1>for the next 7 days</h1>
+			</header>
+		</div>
+	);
+}
